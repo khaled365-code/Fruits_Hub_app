ed2f8cdf51b81a1eda836f03ce63ab535d8b18f6
Khaled Mohamed <khaledmokhaled123@gmail.com>
2025-08-26 18:17:21 +0300

    feat: add the connect app to firebase , add firebase core and cloud firestore

--- lib/features/auth/presentation/manager/signup_cubit/signup_state.dart
+++ lib/features/auth/presentation/manager/signup_cubit/signup_state.dart
@@ -1,4 +1,4 @@
-part of 'signup_cubit.dart';
+ part of 'signup_cubit.dart';
 
 @immutable
 final class SignupState extends Equatable
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_commerce_app/core/bloc_observer/my_bloc_observer.dart';
 import 'package:fruits_commerce_app/core/service_locator/service_locator.dart';
 import 'package:fruits_commerce_app/core/services/cache_service.dart';
+import 'package:fruits_commerce_app/firebase_options.dart';
 
 import 'fruits_shop_app.dart';
 
@@ -10,7 +12,12 @@ void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
   setUpLocator();
-  await CacheService().init();
+  await Future.wait([
+  CacheService().init(),
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  )
+  ]);
   runApp(const FruitsShopApp());
   Bloc.observer=FruitsHubBlocObserver();
 
